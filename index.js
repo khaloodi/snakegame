8c726941c58fbf8b3c3e5a1105cb311c48367c98
Khaled Adad <khaledadad@lewisu.edu>
2020-11-20 22:10:50 -0600

    Remove apple is working and score updating

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ let squares = [];
 let currentSnake = [2, 1, 0];
 let direction = 1;
 let width = 10;
+let count = 0;
 
 function createGrid() {
     for (let i = 0; i < 100; i++) {
@@ -33,7 +34,7 @@ function generateApples() {
     } while (squares[appleIndex].classList.contains('snake'))
     squares[appleIndex].classList.add('apple')
 }
-generateApples()
+generateApples();
 
 function move() {
 
@@ -46,24 +47,6 @@ function move() {
     )
         return clearInterval(timerId)
 
-
-
-    //deal with snake head getting the apple
-    if () {
-        //remove the class of apple
-
-        //grow our snake by adding class of snake to it
-
-        //grow our snake array
-
-        //generate a new apple
-
-        //add one to the score
-
-        //speed up our snake
-
-    }
-
     // remove last element from currentSnake array
     let tail = currentSnake.pop();
     // remove style from element
@@ -72,6 +55,23 @@ function move() {
     currentSnake.unshift(currentSnake[0] + direction);
     // add style
     squares[currentSnake[0]].classList.add('snake');
+
+
+    //deal with snake head getting the apple
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        //remove the class of apple
+        squares[currentSnake[0]].classList.remove('apple');
+        //grow our snake by adding class of snake to it
+        squares[tail].classList.add('snake');
+        //grow our snake array
+        currentSnake.push(tail);
+        //generate a new apple
+        generateApples();
+        //add one to the score
+        count += 1;
+        score.innerText = count;
+        //speed up our snake
+    }
 }
 
 move();
